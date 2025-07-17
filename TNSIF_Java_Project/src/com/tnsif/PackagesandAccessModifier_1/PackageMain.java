f99d7ef05ace07874399f90989153133f0fc0717
user <user@G-sys22>
2025-07-17 14:18:10 +0530

    commit 1

---
+++ TNSIF_Java_Project/src/com/tnsif/PackagesandAccessModifier_1/PackageMain.java
@@ -0,0 +1,14 @@
+package com.tnsif.PackagesandAccessModifier_1;
+
+import com.tnsif.packagesandaccessmodifiers.PackageExample;
+
+public class PackageMain {
+
+	public static void main(String[] args) {
+
+		PackageExample pe = new PackageExample();
+		pe.display();
+
+	}
+
+}
