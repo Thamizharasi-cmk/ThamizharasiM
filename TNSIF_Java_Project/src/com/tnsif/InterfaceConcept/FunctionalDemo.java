342d760b4304f3b0ac2c689fc462c86315a9d66d
user <user@G-sys22>
2025-07-17 16:11:11 +0530

    commit 2

--- TNSIF_Java_Project/src/com/tnsif/InterfaceConcept/FunctionalDemo.java
+++ TNSIF_Java_Project/src/com/tnsif/InterfaceConcept/FunctionalDemo.java
@@ -1,2 +1,17 @@
 package com.tnsif.InterfaceConcept;
 
+public class FunctionalDemo {
+
+	public static void main(String[] args) {
+	
+		//Lambda Expression
+		GreetingDemo gd = () -> 
+		System.out.println("Hello Everyone, Welcome to the Java Session");
+		
+		gd.greet();
+		
+		
+
+	}
+
+}
